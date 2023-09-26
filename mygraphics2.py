47471d63fcedf076021258a92a658c1a5b4dfb4c
MohamedWx <medooka203@gmail.com>
2023-09-26 18:37:28 +0300

    v2 init

---
+++ mygraphics2.py
@@ -0,0 +1,21 @@
+import pygame
+import sys
+import random
+from enum import Enum
+import uuid
+
+
+class Circle(object):
+    def __init__(self):
+        self.__uuid = uuid.uuid4()
+
+
+    def get_uuid(self):
+        return self.__uuid
+
+
+def MyGraphics(object):
+    def __init__(self):
+        pass
+    
+    
